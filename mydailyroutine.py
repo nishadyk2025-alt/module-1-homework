9353a97d52f1c102a506bf1c3c39dd2ca3236075
nishadyk2025-alt <nishad.y.k2025@gmail.com>
2026-07-13 17:13:17 +0530

    dailyroutineprinter

---
+++ mydailyroutine.py
@@ -0,0 +1,6 @@
+#my daily routine
+print("I wake up at 7:30")
+print("I have my breakfast and get ready for school")
+print("I attend classes and come back from school")
+print("I eat dinner and do my home work")
+print("I go to bed at 10:00")

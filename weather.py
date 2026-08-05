a795d7b09a8cb98c9b09220e749410ad6d7311a0
nishadyk2025-alt <nishad.y.k2025@gmail.com>
2026-08-05 10:05:08 +0530

    weather

---
+++ weather.py
@@ -0,0 +1,5 @@
+temp = int(input("What is the temperature today in degrees celcius? "))
+if temp<=10:
+    print("Wear a jacket, it is cold outside")
+else:
+    print("You may wear light clothes.")
